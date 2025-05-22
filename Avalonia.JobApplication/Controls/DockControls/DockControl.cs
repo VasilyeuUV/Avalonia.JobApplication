a6ee069f5ce5a4477f0b11f7b288d284edf9aeeb
VasilyeuUV <vasilyeu.uv@mail.ru>
2025-05-22 12:20:36 +0300

    DockControl

---
+++ Avalonia.JobApplication/Controls/DockControls/DockControl.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+
+namespace Avalonia.JobApplication.Controls.DockControls;
+
+public class DockControl : DockPanel
+{
+    /// <summary>
+    /// Default Constructor.
+    /// </summary>
+    public DockControl()
+    {
+    }
+
+
+
+}
